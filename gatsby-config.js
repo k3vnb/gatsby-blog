6dc2e7ce7cbf1e521c0dd55d1b3486e041cfe917
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-06-01 19:35:56 -0700

    render md files to page

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         path: `${__dirname}/src/markdown`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
