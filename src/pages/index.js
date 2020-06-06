285bdcc94b0f605a43b860c366a0b100ffec2e5d
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-06-05 19:52:16 -0700

    add links and styled components, add sort query to graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const BlogLink = styled(Link)`
+text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
 export default ({ data }) => {
-  console.log(data)
   return (
     <Layout>
       <SEO title="Home" />
       <div>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
             <p>{node.excerpt}</p>
           </div>
         ))}
@@ -35,7 +46,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields:  [frontmatter___date], order: DESC}) {
       edges {
         node {
           html
@@ -44,6 +55,9 @@ export const query = graphql`
             date(fromNow: true)
             description
             title
+          }
+          fields {
+            slug
           }
           excerpt(format: PLAIN)
         }
